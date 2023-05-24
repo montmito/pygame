c0e9e3f2512f6107e9bcefbbd51952ef43209aeb
felipebns <felipebns@al.insper.edu.br>
2023-05-24 10:59:19 -0300

    gravidade

--- XD_FIGHTER.py
+++ XD_FIGHTER.py
@@ -7,18 +7,18 @@ pygame.mixer.init()
 chao = 350
 comprimento = 800
 altura = 600
-ty = 50
 window = pygame.display.set_mode((comprimento, altura))
 pygame.display.set_caption('XD fighter')
-
+g = 5
 #gerando imagem(fd,cr7,jb)
 
-Fd = pygame.image.load('Imagens pygame/fd_pixel2.png').convert()
-cr7 = pygame.image.load('Imagens pygame/cr7_neutro_pixel.png').convert()
-jb = pygame.image.load('Imagens pygame/jb_n_e.png').convert()
+Fd = pygame.image.load('Imagens pygame/fd_pixel2.png').convert_alpha()
+cr7 = pygame.image.load('Imagens pygame/cr7_neutro_pixel.png').convert_alpha()
+jb = pygame.image.load('Imagens pygame/jb_n_e.png').convert_alpha()
 cr7 = pygame.transform.scale(cr7, (75, 75))
 Fd = pygame.transform.scale(Fd, (comprimento, altura))
 jb = pygame.transform.scale(jb,(75, 75))
+
 #classes
 class CR7(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -32,20 +32,17 @@ class CR7(pygame.sprite.Sprite):
         self.speedy = 0
         self.movey = 0
     def update(self):
+        
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        self.speedy = 0
-        if self.rect.y < chao:
-            self.rect.y += 1
+        self.speedy += g
+        #self.speedy = 0
+        if self.rect.bottom >= chao:
+            self.rect.y = chao
         if self.rect.x > 600:
             self.rect.x = 600
         if self.rect.x < 150:
             self.rect.x = 150
-    def gravity(self):
-        self.movey += 3.2 # how fast player falls
-        if self.rect.y < chao and self.movey >= 0:
-            self.movey = 0
-            self.rect.y = chao-ty-ty
 class JB(pygame.sprite.Sprite):
     def __init__(self, img):
         pygame.sprite.Sprite.__init__(self)
@@ -59,29 +56,33 @@ class JB(pygame.sprite.Sprite):
         self.movey = 0
 
     def update(self):
+
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        self.speedy = 0
+        self.speedy += g
+        #self.speedy = 0
 
-        if self.rect.y < chao:
-            self.rect.y += 1
+        if self.rect.bottom >= chao:
+            self.rect.y = chao
         if self.rect.x > 600:
             self.rect.x = 600
         if self.rect.x < 150:
             self.rect.x = 150
             
-    def gravity(self):
-        self.movey += 3.2 # how fast player falls
-        if self.rect.y < chao and self.movey >= 0:
-            self.movey = 0
-            self.rect.y = chao-ty-ty
+
 #começo !!!
 
 game = True
+#ajustes
+clock = pygame.time.Clock()
+FPS = 30
+
+
 lutador1 = CR7(cr7)
 lutador2 = JB(jb)
 
 while game:
+    clock.tick(FPS)
     #eventos
     for event in pygame.event.get():
         #consequências
@@ -91,37 +92,37 @@ while game:
         if event.type == pygame.KEYDOWN:
             # Dependendo da tecla, altera a velocidade.
             if event.key == pygame.K_LEFT:
-                lutador1.speedx -= 1
+                lutador1.speedx -= 25
             if event.key == pygame.K_RIGHT:
-                lutador1.speedx += 1
+                lutador1.speedx += 25
             if event.key == pygame.K_UP:
-                lutador1.speedy -= 150
+                lutador1.speedy = -50
+                lutador1.rect.y += -30
             if event.key == pygame.K_w:
-                lutador2.speedy -= 150
+                lutador2.speedy = -50
+                lutador2.rect.y += -30
             if event.key == pygame.K_a:
-                lutador2.speedx -= 1
+                lutador2.speedx -= 25
             if event.key == pygame.K_d:
-                lutador2.speedx += 1
+                lutador2.speedx += 25
         # Verifica se soltou alguma tecla.
         if event.type == pygame.KEYUP:
             # Dependendo da tecla, altera a velocidade.
             if event.key == pygame.K_LEFT:
-                lutador1.speedx += 1
+                lutador1.speedx += 25
             if event.key == pygame.K_RIGHT:
-                lutador1.speedx -= 1
-            if event.key == pygame.K_w:
-                lutador2.speedy += 1
-            if event.key == pygame.K_UP:
-                lutador1.speedy += 1
+                lutador1.speedx -= 25
+            # if event.key == pygame.K_w:
+            #     lutador2.speedy += 100
+            # if event.key == pygame.K_UP:
+            #     lutador1.speedy += 100
             if event.key == pygame.K_a:
-                lutador2.speedx += 1
+                lutador2.speedx += 25
             if event.key == pygame.K_d:
-                lutador2.speedx -= 1
+                lutador2.speedx -= 25
 
     window.blit(Fd,(0,0))
     #atualiza
-    # lutador1.gravity()
-    # lutador2.gravity()
     lutador1.update()
     lutador2.update()
     window.blit(lutador1.image, lutador1.rect)
